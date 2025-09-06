3024f9d8078f50b1aabb95dd4bb7fca208d3b585
amrit achhami <amritachhami014@gmail.com>
2025-09-06 22:29:57 +0545

    first commit

---
+++ main.renew_service/src/main/java/com/ram/renew_service/main/renew_service/Application.java
@@ -0,0 +1,13 @@
+package com.ram.renew_service.main.renew_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
